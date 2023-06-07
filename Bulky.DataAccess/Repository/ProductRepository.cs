d2e043da92c2e5de24f221ebcc82f1d12078655b
Sanjatul Hasan Siam <sanjatulhasansiam.iit@gmail.com>
2023-06-07 12:56:50 +0600

    Product Crud Operation Added

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bookish.DataAccess.Data;
+using Bookish.DataAccess.Repository.IRepository;
+using Bookish.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookish.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+     
+       
+    }
+}
