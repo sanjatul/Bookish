94fcb69b7bc8cb6f5ab6abbe86e696bcb0917578
Sanjatul Hasan Siam <sanjatulhasansiam.iit@gmail.com>
2023-06-07 11:04:16 +0600

    Category Features Added with N Tier Architecture And Repository Pattern

--- Bulky.Models/ErrorViewModel.cs
+++ Bulky.Models/ErrorViewModel.cs
@@ -1,4 +1,7 @@
-namespace BulkyWeb.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Bulky.Models
 {
     public class ErrorViewModel
     {
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,8 +1,16 @@
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("Conn")
+    )) ;
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
